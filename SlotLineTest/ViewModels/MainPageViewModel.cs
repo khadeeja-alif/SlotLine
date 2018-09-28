18df5a47f2bb665831b7507aec0385e299a2c63e
Khadeeja Alif <khadeeja@fantacode.com>
2018-09-28 22:11:30 +0530

    Linked Data Library

--- SlotLineTest/ViewModels/MainPageViewModel.cs
+++ SlotLineTest/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Commands;
+﻿using DataSlotLine;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
@@ -10,6 +11,7 @@ namespace SlotLineTest.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public List<Class1> MyData { get; set; }
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
